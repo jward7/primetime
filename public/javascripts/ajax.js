4da84706108622d0875d458ea030a912225c3a8e
Justin Ward <jward@modernmagnetic.com>
2015-04-13 17:10:00 +1000

    added JS libraries and required gems.

---
+++ public/javascripts/ajax.js
@@ -0,0 +1,25 @@
+var request  = new XMLHttpRequest();
+
+/*request.onreadystatechange = function() {
+
+*//*    console.log('ready state has changed');
+    console.log('readyState', this.readyState);
+    console.log('readyText', this.responseText);*//**//*
+
+
+    if (this.readyState === 4) { // ready state is 1-4.
+        console.log('Done');
+        document.write(this.responseText);
+    }
+};
+
+request.open('GET', '/slow');
+request.send();
+
+console.log('AJAX request is probably still running')*/
+
+$(document).ready(function() {
+    $.getJSON('/api/students').done(function() {
+        console.log('done');
+    });
+});
