cbc7162142bd288c580023815a8aaba540ae5441
Justin Ward <jward@modernmagnetic.com>
2015-04-14 20:46:27 +1000

    got main index page showing dummy data

--- main.rb
+++ main.rb
@@ -13,9 +13,20 @@ require_relative 'database'
 Database.clear_tables
 Database.seed
 
+# root route
 get '/' do
+  @p = Prime.all.last.prime_c
+  @s1 = Speed.all.last.person
+  Speed.create speed_c: 77, person: 'Kevin'
+  @s2 = Speed.all.last.person
+  #Speed.create speed_c: 66, person: 'Arnold', computer_c: 'Mac Book Pro'
+  #@s3 = Speed.all.last.computer_c # todo here trying to get associate working
+  #@s4 = Computer.speed.create()
+  @temp = [1,2,3,4,5]
   erb :index
 end
+
+# make 5 RESTFUL routes
 
 
 
